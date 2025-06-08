1b9b0ec728d323c96342bd6ae19318eb37c47256
Zhou <399400001@qq.com>
2025-06-09 02:51:34 +0800

    #2025_06_08 home,实现SlimSerialExecutor接口，未添加测试...

--- ZhousThreadPool_v001/SlimSerialExecutor.cpp
+++ ZhousThreadPool_v001/SlimSerialExecutor.cpp
@@ -1,125 +1,389 @@
 #include "SlimSerialExecutor.h"
 
 namespace CustomerDefined {
-/************************************************
- * SlimSerialExecutor::PriorityTimer ，定义 *
- ************************************************
+/************************************
+ * class SlimSerialExecutor::PriorityTimerImpl 定义 *
+ ************************************
  */
-SlimSerialExecutor::PriorityTimer::PriorityTimer()
-    : TaskItemPQueue{}, RepeatedTaskIdUSet{}, InternalThreadUPtr{nullptr},
-      InternalMutex{}, CondVar{}, RunningTag{false}, PausedTag{false},
-      RepeatedTaskId{0} {}
-//
-SlimSerialExecutor::PriorityTimer::~PriorityTimer() { Stop(); }
-//
-void SlimSerialExecutor::PriorityTimer::Start() {
-  if (RunningTag.load())
-    return;
+class SlimSerialExecutor::PriorityTimerImpl {
+public:
+  /*struct PriorityTaskItem
+   * 优先级队列中维护的项
+   * 以时间点为权与任务函数关联
+   * 内部使用
+   */
+  struct PriorityTaskItem {
+    using OriginTaskItem_Ty = TaskItem_Ty;
+    //
+    TimePoint_Ty StartTp;
+    //
+    OriginTaskItem_Ty TaskItem;
+    //
+    RepeatedTaskId_Ty RepeatedId;
+    //
+    PriorityTaskItem() = delete;
+    //
+    PriorityTaskItem(const PriorityTaskItem &) = default;
+    //
+    PriorityTaskItem(const TimePoint_Ty stp, OriginTaskItem_Ty &&otitem,
+                     const RepeatedTaskId_Ty rtId = InvalidRepeatedTaskId)
+        : StartTp{stp}, TaskItem{std::move(otitem)}, RepeatedId{rtId} {}
+    //
+    ~PriorityTaskItem() {}
+    //
+    PriorityTaskItem &operator=(const PriorityTaskItem &) = default;
+    //
+    bool operator<(const PriorityTaskItem &other) const {
+      return StartTp > other.StartTp;
+    }
+    //
+    void operator()() { TaskItem(); }
+  }; // struct PriorityTaskItem
+  /* enum class State
+   * 标记Timer内部状态
+   */
+  enum class RunnerState { Stopped, Running, Paused }; // enum class State
+  std::string GetRunnerStateString(const RunnerState &tar) {
+    switch (tar) {
+    case RunnerState::Running:
+      return "RunnerState::Running";
+    case RunnerState::Stopped:
+      return "RunnerState::Stopped";
+    case RunnerState::Paused:
+      return "RunnerState::Paused";
+    default:
+      return "Unknow State";
+    } // switch
+  }
+  // 内部成员
+  // 任务队列
+  std::priority_queue<PriorityTaskItem> TaskItemPQueue;
+  // 管理重复任务Id
+  std::unordered_set<RepeatedTaskId_Ty> RepeatedIdUSet;
+  // 执行线程包装器指针
+  TaskRunnerUPtr_Ty TaskRunnerUPtr;
+  // 锁
+  std::mutex InternalMutex;
+  std::condition_variable CondVar;
   //
+  std::atomic<RunnerState> State;
+  //
+  std::atomic<RepeatedTaskId_Ty> ValidRepeatedTaskId;
+  //
+  PriorityTimerImpl()
+      : TaskItemPQueue{}, RepeatedIdUSet{}, TaskRunnerUPtr{nullptr},
+        InternalMutex{}, CondVar{}, State{RunnerState::Stopped},
+        ValidRepeatedTaskId{0} {}
+  //
+  PriorityTimerImpl(const PriorityTimerImpl &) = delete;
+  //
+  ~PriorityTimerImpl() {}
+  //
+  PriorityTimerImpl &operator=(const PriorityTimerImpl &) = delete;
+  //
+  void Start();
+  //
+  void Stop();
+  //
+  void Pause();
+  //
+  void Pause(Duration_Ty timeOffMicroSec);
+  //
+  void Resume();
+  //
+  void AddRealTimeTask(TaskItem_Ty &&titem);
+  //
+  void AddNormalTask(TaskItem_Ty &&titem, Duration_Ty delayMicroSec);
+  //
+  [[nodiscard]] RepeatedTaskId_Ty AddNormalTask(TaskItem_Ty &&titem,
+                                                Duration_Ty delayMicroSec,
+                                                Duration_Ty periodMicroSec,
+                                                uint64_t repCount);
+  //
+  bool TryCancelTask(RepeatedTaskId_Ty rtid);
+  //
+  void DeleteAllTask();
+  // Utility
+  RepeatedTaskId_Ty GetRepeatedId() { return ValidRepeatedTaskId++; }
+  // 线程函数
+  void ThreadFunc();
+  //
+  void PostRepeatedTask(TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid,
+                        Duration_Ty delayMicroSec, Duration_Ty periodMicroSec,
+                        uint64_t repCount);
+  //
+  void ExecuteAndAddNext(TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid,
+                         Duration_Ty periodMicroSec, uint64_t repCount);
+}; // class SlimSerialExecutor::PriorityTimerImpl
+//
+void SlimSerialExecutor::PriorityTimerImpl::Start() {
+  std::lock_guard<std::mutex> lk(InternalMutex);
+  if (State.load() != RunnerState::Stopped)
+    return;
+  State.store(RunnerState::Running);
   for (; !TaskItemPQueue.empty();)
     TaskItemPQueue.pop();
-  RepeatedTaskIdUSet.clear();
-  //
-  RepeatedTaskId.store(0);
-  InternalThreadUPtr =
-      std::make_unique<ThreadWapper>([this]() { this->DefThreadFunc(); });
-  RunningTag.store(true);
-  PausedTag.store(false);
+  RepeatedIdUSet.clear();
+  ValidRepeatedTaskId.store(0);
+  TaskRunnerUPtr =
+      std::make_unique<TaskRunner_Ty>([this]() { this->ThreadFunc(); });
 }
 //
-void SlimSerialExecutor::PriorityTimer::ReStart() {
-  if (RunningTag.load() || PausedTag.load())
+void SlimSerialExecutor::PriorityTimerImpl::Stop() {
+  {
+    std::lock_guard<std::mutex> lk(InternalMutex);
+    if (State.load() == RunnerState::Stopped)
+      return;
+    for (; !TaskItemPQueue.empty();)
+      TaskItemPQueue.pop();
+    RepeatedIdUSet.clear();
+    ValidRepeatedTaskId.store(0);
+    State.store(RunnerState::Stopped);
+    CondVar.notify_one();
+  }
+  TaskRunnerUPtr.reset();
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::Pause() {
+  std::lock_guard<std::mutex> lk(InternalMutex);
+  if (State.load() != RunnerState::Running)
     return;
-  PausedTag.store(false);
+  State.store(RunnerState::Paused);
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::Pause(Duration_Ty timeOffMicroSec) {
+  AddRealTimeTask([this, timeOffMicroSec]() {
+    {
+      std::lock_guard<std::mutex> lk(this->InternalMutex);
+      if (State.load() != RunnerState::Running)
+        return;
+      State.store(RunnerState::Paused);
+    }
+    std::this_thread::sleep_for(timeOffMicroSec);
+    {
+      std::lock_guard<std::mutex> lk(this->InternalMutex);
+      if (State.load() != RunnerState::Paused)
+        return;
+      State.store(RunnerState::Paused);
+    }
+  });
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::Resume() {
+  std::lock_guard<std::mutex> lk(InternalMutex);
+  if (State.load() != RunnerState::Paused)
+    return;
+  State.store(RunnerState::Running);
   CondVar.notify_one();
 }
 //
-void SlimSerialExecutor::PriorityTimer::Pause() {
-  if (RunningTag.load() || !PausedTag.load())
+void SlimSerialExecutor::PriorityTimerImpl::AddRealTimeTask(
+    TaskItem_Ty &&titem) {
+  AddNormalTask(std::move(titem), Duration_Ty(0));
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::AddNormalTask(
+    TaskItem_Ty &&titem, Duration_Ty delayMicroSec) {
+  auto startTp = Clock_Ty::now() + delayMicroSec;
+  bool notifyTag = true;
+  {
+    std::lock_guard<std::mutex> lk(InternalMutex);
+    notifyTag =
+        TaskItemPQueue.empty() ? true : startTp <= TaskItemPQueue.top().StartTp;
+    TaskItemPQueue.push(std::move(PriorityTaskItem(startTp, std::move(titem))));
+  }
+  if (notifyTag && State.load() == RunnerState::Running)
+    CondVar.notify_one();
+}
+//
+[[nodiscard]] SlimSerialExecutor::RepeatedTaskId_Ty
+SlimSerialExecutor::PriorityTimerImpl::AddNormalTask(TaskItem_Ty &&titem,
+                                                     Duration_Ty delayMicroSec,
+                                                     Duration_Ty periodMicroSec,
+                                                     uint64_t repCount) {
+  RepeatedTaskId_Ty resId = GetRepeatedId();
+  {
+    std::lock_guard<std::mutex> lk(InternalMutex);
+    RepeatedIdUSet.insert(resId);
+  }
+  PostRepeatedTask(std::move(titem), resId, delayMicroSec, periodMicroSec,
+                   repCount);
+  return resId;
+}
+//
+bool SlimSerialExecutor::PriorityTimerImpl::TryCancelTask(
+    RepeatedTaskId_Ty rtid) {
+  std::lock_guard<std::mutex> lk(InternalMutex);
+  if (State.load() == RunnerState::Stopped)
+    return false;
+  auto fIter = RepeatedIdUSet.find(rtid);
+  if (fIter == RepeatedIdUSet.end())
+    return false;
+  RepeatedIdUSet.erase(rtid);
+  return true;
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::DeleteAllTask() {
+  std::lock_guard<std::mutex> lk(InternalMutex);
+  for (; !TaskItemPQueue.empty();)
+    TaskItemPQueue.pop();
+  RepeatedIdUSet.clear();
+  ValidRepeatedTaskId.store(0);
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::ThreadFunc() {
+  for (;;) {
+    std::unique_lock<std::mutex> ulk(InternalMutex);
+    CondVar.wait(ulk, [this]() {
+      return State.load() == RunnerState::Stopped ||
+             (!TaskItemPQueue.empty() && State == RunnerState::Running);
+    });
+    if (State.load() == RunnerState::Stopped)
+      break;
+    if (State.load() == RunnerState::Paused) {
+      CondVar.wait(ulk,
+                   [this]() { return State.load() != RunnerState::Paused; });
+      continue;
+    }
+    if (TaskItemPQueue.empty())
+      continue;
+    auto nowTp = Clock_Ty::now();
+    auto nextStartTp = TaskItemPQueue.top().StartTp;
+    if (nextStartTp > nowTp) {
+      CondVar.wait_until(ulk, nextStartTp);
+      continue;
+    }
+    PriorityTaskItem pitem = TaskItemPQueue.top();
+    ulk.unlock();
+    pitem();
+  } // for-loop
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::PostRepeatedTask(
+    TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid, Duration_Ty delayMicroSec,
+    Duration_Ty periodMicroSec, uint64_t repCount) {
+  auto startTp = Clock_Ty::now() + delayMicroSec;
+  bool notifyTag = true;
+  {
+    std::lock_guard<std::mutex> lk(InternalMutex);
+    auto fIter = RepeatedIdUSet.find(rtid);
+    if (fIter == RepeatedIdUSet.end())
+      return;
+    if (repCount == 0) {
+      RepeatedIdUSet.erase(fIter);
+      return;
+    }
+    auto postFunc = [this, &titem, rtid, periodMicroSec, repCount]() {
+      this->ExecuteAndAddNext(std::move(titem), rtid, periodMicroSec, repCount);
+    };
+    notifyTag =
+        TaskItemPQueue.empty() ? true : startTp <= TaskItemPQueue.top().StartTp;
+    TaskItemPQueue.push(
+        std::move(PriorityTaskItem(startTp, std::move(postFunc), rtid)));
+  } // lk
+  if (notifyTag && State.load() == RunnerState::Running)
+    CondVar.notify_one();
+}
+//
+void SlimSerialExecutor::PriorityTimerImpl::ExecuteAndAddNext(
+    TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid, Duration_Ty periodMicroSec,
+    uint64_t repCount) {
+  titem();
+  auto nextStartTp = Clock_Ty::now() + periodMicroSec;
+  bool notifyTag = true;
+  uint64_t lastRepCount = (repCount == SlimSerialExecutor::InfiniteRepeated)
+                              ? SlimSerialExecutor::InfiniteRepeated
+                              : repCount - 1;
+  auto postFunc = [this, &titem, rtid, periodMicroSec, lastRepCount]() {
+    this->PostRepeatedTask(std::move(titem), rtid, periodMicroSec,
+                           periodMicroSec, lastRepCount);
+  };
+  {
+    std::lock_guard<std::mutex> lk(InternalMutex);
+    notifyTag = TaskItemPQueue.empty()
+                    ? true
+                    : nextStartTp <= TaskItemPQueue.top().StartTp;
+    TaskItemPQueue.push(
+        PriorityTaskItem(nextStartTp, std::move(postFunc), rtid));
+  } // lk
+  if (notifyTag && State.load() == RunnerState::Running)
+    CondVar.notify_one();
+}
+/************************************
+ * SlimSerialExecutor，定义 *
+ * 外部接口封装 *
+ ************************************
+ */
+SlimSerialExecutor::PriorityTimer::PriorityTimer() : PTimerUPtr{nullptr} {}
+//
+SlimSerialExecutor::PriorityTimer::~PriorityTimer() {}
+//
+void SlimSerialExecutor::PriorityTimer::Start() {
+  if (!PTimerUPtr)
+    PTimerUPtr =
+        std::make_unique<PriorityTimerImpl>(std::move(PriorityTimerImpl()));
+  PTimerUPtr->Start();
+}
+//
+void SlimSerialExecutor::PriorityTimer::Stop() {
+  if (!PTimerUPtr)
     return;
-  PausedTag.store(true);
+  PTimerUPtr->Stop();
+  PTimerUPtr.reset();
+}
+//
+void SlimSerialExecutor::PriorityTimer::Resume() {
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->Resume();
+}
+//
+void SlimSerialExecutor::PriorityTimer::Pause() {
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->Pause();
 }
 //
 void SlimSerialExecutor::PriorityTimer::Pause(
     const std::chrono::microseconds &timeoffMicroSec) {
-  AddRealTimeTask([this, timeoffMicroSec]() { StayAWhile(timeoffMicroSec); });
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->Pause(timeoffMicroSec);
 }
 //
-void SlimSerialExecutor::PriorityTimer::AddRealTimeTask(TaskItem_Ty titem) {
-  // 这里我没加锁，如果加锁需要递归锁？
-  auto nowTp = Clock_Ty::now();
-  auto topItemTp = TaskItemPQueue.top().StartTp;
-
-  auto duMicroSec =
-      (nowTp > topItemTp)
-          ? std::chrono::duration_cast<std::chrono::microseconds>(
-                topItemTp - nowTp - std::chrono::microseconds(100))
-          : std::chrono::microseconds(-1);
-  AddNormalTask(titem, duMicroSec);
+void SlimSerialExecutor::PriorityTimer::AddRealTimeTask(TaskItem_Ty &&titem) {
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->AddRealTimeTask(std::move(titem));
 }
 //
 void SlimSerialExecutor::PriorityTimer::AddNormalTask(
-    TaskItem_Ty titem, const std::chrono::microseconds &delayedMicroSec) {
-  auto startTp = Clock_Ty::now() + delayedMicroSec;
-  std::unique_lock<std::mutex> ulk(InternalMutex);
-  TaskItemPQueue.push(std::move(PriorityTaskItem(startTp, std::move(titem))));
-  CondVar.notify_one();
-}
-//
-[[nodiscard]] uint64_t SlimSerialExecutor::PriorityTimer::AddNormalTask(
-    TaskItem_Ty titem, const std::chrono::microseconds &delayedMicroSec,
-    const std::chrono::microseconds &periodMicroSec, uint64_t repCount) {
-  RepeatedTaskId_Ty curTaskId = GetNextRepeatedTaskId();
-  RepeatedTaskIdUSet.insert(curTaskId);
-  InternalAddNormalTask(std::move(titem), curTaskId, delayedMicroSec,
-                        periodMicroSec, repCount);
-  return curTaskId;
-}
-
-// private members
-void SlimSerialExecutor::PriorityTimer::DefThreadFunc() {}
-//
-void SlimSerialExecutor::PriorityTimer::StayAWhile(
-    const std::chrono::microseconds &timeoffMicroSec) {
-  PausedTag.store(true);
-  std::this_thread::sleep_for(timeoffMicroSec);
-  PausedTag.store(false);
-}
-//
-SlimSerialExecutor::RepeatedTaskId_Ty
-SlimSerialExecutor::PriorityTimer::GetNextRepeatedTaskId() {
-  return RepeatedTaskId++;
-}
-//
-void SlimSerialExecutor::PriorityTimer::InternalAddNormalTask(
-    TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid,
-    const std::chrono::microseconds &delayedMicroSec,
-    const std::chrono::microseconds &periodMicroSec, uint64_t repCount) {
-  if (RepeatedTaskIdUSet.find(rtid) == RepeatedTaskIdUSet.end())
+    TaskItem_Ty &&titem, Duration_Ty delayMicroSec) {
+  if (!PTimerUPtr)
     return;
-  if (repCount == 0) {
-    RepeatedTaskIdUSet.erase(rtid);
-    return;
-  }
-  //
-  TaskItem_Ty transTaskItem = [this, &titem, rtid, periodMicroSec, repCount]() {
-    this->TaskExecuteAndAddNext(std::move(titem), rtid, periodMicroSec,
-                                repCount);
-  };
-  auto transStartTp = Clock_Ty::now() + delayedMicroSec;
-  {
-    std::unique_lock<std::mutex> ulk(InternalMutex);
-    TaskItemPQueue.push(
-        std::move(PriorityTaskItem(transStartTp, std::move(transTaskItem))));
-  }
-  CondVar.notify_one();
+  PTimerUPtr->AddNormalTask(std::move(titem), delayMicroSec);
 }
 //
-void SlimSerialExecutor::PriorityTimer::TaskExecuteAndAddNext(
-    TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid,
+[[nodiscard]] SlimSerialExecutor::RepeatedTaskId_Ty
+SlimSerialExecutor::PriorityTimer::AddNormalTask(
+    TaskItem_Ty &&titem, const std::chrono::microseconds &delayMicroSec,
     const std::chrono::microseconds &periodMicroSec, uint64_t repCount) {
-  titem();
-  InternalAddNormalTask(titem,rtid,periodMicroSec,periodMicroSec,repCount-1);
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->AddNormalTask(std::move(titem), delayMicroSec, periodMicroSec,
+                            repCount);
 }
 //
+bool SlimSerialExecutor::PriorityTimer::TryCancelSpecifiedTask(
+    RepeatedTaskId_Ty rtid) {
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->TryCancelTask(rtid);
+}
+//
+void SlimSerialExecutor::PriorityTimer::DeleteAllTasks() {
+  if (!PTimerUPtr)
+    return;
+  PTimerUPtr->DeleteAllTask();
+}
 } // namespace CustomerDefined
--- ZhousThreadPool_v001/SlimSerialExecutor.h
+++ ZhousThreadPool_v001/SlimSerialExecutor.h
@@ -7,6 +7,7 @@
 #include <queue>
 #include <unordered_set>
 //
+#include <future>
 #include <mutex>
 //
 namespace CustomerDefined {
@@ -20,64 +21,23 @@ public:
   //
   using Clock_Ty = std::chrono::high_resolution_clock;
   using TimePoint_Ty = std::chrono::time_point<Clock_Ty>;
+  using Duration_Ty = std::chrono::microseconds;
+  //
   using TaskItem_Ty = std::function<void()>;
   //
   static constexpr const uint64_t InfiniteRepeated{-1};
   static constexpr const uint64_t InvalidRepeatedTaskId{-1};
 
 private:
-  /*struct PriorityTaskItem
-   * 优先级队列中维护的项
-   * 以时间点为权与任务函数关联
-   * 内部使用
-   */
-  struct PriorityTaskItem {
-    using OriginTaskItem_Ty = TaskItem_Ty;
-    //
-    TimePoint_Ty StartTp;
-    //
-    OriginTaskItem_Ty TaskItem;
-    //
-    RepeatedTaskId_Ty RepeatedId;
-    //
-    PriorityTaskItem() = delete;
-    //
-    PriorityTaskItem(const PriorityTaskItem &) = default;
-    //
-    PriorityTaskItem(const TimePoint_Ty stp, OriginTaskItem_Ty &&otitem,
-                     const RepeatedTaskId_Ty rtId = InvalidRepeatedTaskId)
-        : StartTp{stp}, TaskItem{std::move(otitem)}, RepeatedId{rtId} {}
-    //
-    ~PriorityTaskItem() {}
-    //
-    PriorityTaskItem &operator=(const PriorityTaskItem &) = default;
-    //
-    bool operator<(const PriorityTaskItem &other) const {
-      return StartTp > other.StartTp;
-    }
-  }; // struct PriorityTaskItem
-
+  // PriorityTimerImpl 前向声明
+  // 内部实现
+  class PriorityTimerImpl;
   /* PriorityTimer
    * 维护内部线程和任务队列（以发生时间点为权的优先级队列）
    */
   class PriorityTimer {
   private:
-    //
-    std::priority_queue<PriorityTaskItem> TaskItemPQueue;
-    // 存储重复任务标记,添加重复任务的方式：
-    // 在队列中添加一个工作项，该工作项的内容是添加一个执行repnum-1的任务
-    std::unordered_set<RepeatedTaskId_Ty> RepeatedTaskIdUSet;
-    //
-    std::unique_ptr<ThreadWapper> InternalThreadUPtr;
-    //
-    std::mutex InternalMutex;
-    std::condition_variable CondVar;
-    // true-running;false-stopped
-    std::atomic<bool> RunningTag;
-    // true-Paused;false-running;
-    std::atomic<bool> PausedTag;
-    // 重复任务标记计数
-    std::atomic<RepeatedTaskId_Ty> RepeatedTaskId;
+    std::unique_ptr<PriorityTimerImpl> PTimerUPtr;
 
   public:
     PriorityTimer();
@@ -93,68 +53,75 @@ private:
      * 会清理内部容器
      */
     void Start();
-    /* ReStart;
-     * RunningTag-true,PausedTag-true
-     * 设置PausedTag为false，唤醒休眠的线程
-     * 其余情况均无操作
+    /* Stop
+     * 设置RunningTag为false
+     * 清空内部容器
+     */
+    void Stop();
+    /* Resume
      * 此接口 不 会清理内部容器
      * 如果暂停期间堆积的任务过多需要自行判断是否清理队列
      */
-    void ReStart();
+    void Resume();
     /* Pause
      * 挂起线程 一段时间
      * 指定时间其实是添加一项实时任务
      */
     void Pause();
     void Pause(const std::chrono::microseconds &timeoffMicroSec);
-    /* Stop
-     * 设置RunningTag为false
-     * 清空内部容器
+    /* Add系列
+     * 添加常规（无参数）任务
+     * 如果不指定周期和重复次数默认重复1次（接口1）
+     * 不会返回RepeatedTaskId，所以不会记录为重复任务，也不能取消
+     * （接口2）可以根据需要管理RepeatedTaskId
+     * 可以尝试取消该重复任务，但已经添加进队列中的任务不会取消
+     * 为repCount指定 InfiniteRepeated 周期性重复直到取消为止
      */
-    void Stop();
-    /* AddRealTimeTask
-     * 添加一个立即（最高优先级）执行的任务进队列
-     */
-    void AddRealTimeTask(TaskItem_Ty titem);
-    /* AddNormalTask
-     * 添加常规任务
-     * 如果不指定周期和重复次数默认重复1次
-     * 不进RepeatedTaskIdUSet
-     * InfiniteRepeated重复直到取消为止
-     */
-    void AddNormalTask(TaskItem_Ty titem,
-                       const std::chrono::microseconds &delayedMicroSec);
-    [[nodiscard]] uint64_t AddNormalTask(
-        TaskItem_Ty titem, const std::chrono::microseconds &delayedMicroSec,
+    void AddRealTimeTask(TaskItem_Ty &&titem);
+    void AddNormalTask(TaskItem_Ty &&titem, Duration_Ty delayMicroSec);
+    //
+    [[nodiscard]] RepeatedTaskId_Ty AddNormalTask(
+        TaskItem_Ty &&titem, const std::chrono::microseconds &delayMicroSec,
         const std::chrono::microseconds &periodMicroSec, uint64_t repCount);
-
-    /* TryCancelRepeatedTask
-     * 尝试删除一个任务
-     * 已开始的任务无法取消
+    /* TryCancelSpecifiedTask
+     * 尝试删除一个已添加的NormalTask,
+     * rtaskId通过AddNormalTask获得
+     * 已进入队列的任务无法取消
      */
     bool TryCancelSpecifiedTask(RepeatedTaskId_Ty rtaskId);
     /* DeleteAllTasts
      * 清空内部容器
      */
-    void DeleteAllTasts();
-
-  private:
-    void DefThreadFunc();
+    void DeleteAllTasks();
+    /* Post系列
+     * 只提供需要参数有返回值任务的转发
+     * 不提供重复任务(封装太麻烦)
+     * 不用返回值时由调用者封装
+     */
+    template <typename Func_Ty, typename... Args_Ty>
+    auto PostRealTimeTask(Func_Ty &&func, Args_Ty... args)
+        -> std::future<std::result_of_t<Func_Ty(Args_Ty...)>> {
+      using Result_Ty = std::result_of_t<Func_Ty(Args_Ty...)>;
+      auto titemSPtr = std::make_shared<std::packaged_task<Result_Ty>>(
+          std::bind(std::forward<Func_Ty>(std::move(func)),
+                    std::forward<Args_Ty>(args)...));
+      Result_Ty resFu = titemSPtr->get_future();
+      AddRealTimeTask([titemSPtr]() { (*titemSPtr)(); })
+    }
     //
-    void StayAWhile(const std::chrono::microseconds &timeoffMicroSec);
-    //
-    RepeatedTaskId_Ty GetNextRepeatedTaskId();
-    //
-    void InternalAddNormalTask(TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid,
-                               const std::chrono::microseconds &delayedMicroSec,
-                               const std::chrono::microseconds &periodMicroSec,
-                               uint64_t repCount);
-    // 此接口用于在添加重复工作项的转发任务到期时
-    void TaskExecuteAndAddNext(TaskItem_Ty &&titem, RepeatedTaskId_Ty rtid,
-                               const std::chrono::microseconds &periodMicroSec,
-                               uint64_t repCount);
-
+    template <typename Du_Ty, typename Func_Ty, typename... Args_Ty>
+    auto PostNormalTask(Du_Ty delay, Func_Ty &&func, Args_Ty &&...args)
+        -> std::future<std::result_of_t<Func_Ty(Args_Ty...)>> {
+      using Result_Ty = std::result_of_t<Func_Ty(Args_Ty...)>;
+      auto titemSPtr = std::make_shared<std::packaged_task<Result_Ty>>(
+          std::bind(std::forward<Func_Ty>(std::move(func)),
+                    std::forward<Args_Ty>(args)...));
+      Result_Ty resFu = titemSPtr->get_future();
+      AddRealTimeTask([titemSPtr, delay]() { (*titemSPtr)(); },
+                      std::chrono::duration_cast<Duration_Ty>(delay));
+    }
   }; // class PriorityTimer
-
+private:
+  PriorityTimer PTimer;
 }; // class SlimSerialExecutor
 } // namespace CustomerDefined
